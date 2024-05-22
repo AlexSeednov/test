0d1c8bb888f10a1f5743565e93fbb69c7c822168
Seednov 'Alex' Alexander <seednov.alexander@gmail.com>
2024-05-22 11:19:50 +0300

    Base upd

--- lib/main.dart
+++ lib/main.dart
@@ -9,11 +9,23 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: Scaffold(
         body: Column(
           children: [
-            Text('Hello World!'),
+            const Text('Test WC app'),
+            FilledButton(
+              onPressed: () => {},
+              child: const Text('Auth directly'),
+            ),
+            FilledButton(
+              onPressed: () => {},
+              child: const Text('Auth via modal'),
+            ),
+            FilledButton(
+              onPressed: () => {},
+              child: const Text('Auth via Web3App'),
+            ),
           ],
         ),
       ),
