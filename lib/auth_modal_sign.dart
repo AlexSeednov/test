ac0106e2ca4b8cbb325f9f2ddf180a24004d0b0c
Seednov 'Alex' Alexander <seednov.alexander@gmail.com>
2024-05-27 20:26:47 +0300

    Simplified

--- lib/auth_modal_sign.dart
+++ lib/auth_modal_sign.dart
@@ -9,45 +9,19 @@ import 'package:web3modal_flutter/web3modal_flutter.dart';
 /// https://github.com/WalletConnect/Web3ModalFlutter/issues/122
 final class AuthModalSign {
   ///
-  static Web3App? _web3app;
-
-  ///
   static W3MService? _w3mService;
 
   ///
   static Future<void> prepare() async {
-    _web3app = await Web3App.createInstance(
-      projectId: walletConnectProjectId,
-      metadata: walletConnectMetadata,
-    );
-
     /// Only Ethereum or Polygon as available chains
     W3MChainPresets.chains.removeWhere(
       (String key, W3MChainInfo value) =>
           value.chainName != 'Ethereum' && value.chainName != 'Polygon',
     );
     _w3mService = W3MService(
-      web3App: _web3app,
       projectId: walletConnectProjectId,
       metadata: walletConnectMetadata,
       includedWalletIds: walletConnectIncluded,
-      /*requiredNamespaces: {
-        'eip155': const W3MNamespace(
-          chains: ['eip155:1'],
-          methods: [
-            MethodConstants.WC_AUTH_REQUEST,
-            "personal_sign",
-          ],
-          events: [
-            "chainChanged",
-            "accountsChanged",
-            "message",
-            "disconnect",
-            "connect",
-          ],
-        ),
-      },*/
-      //optionalNamespaces: {},
     );
 
     await _w3mService!.init();
@@ -55,22 +29,19 @@ final class AuthModalSign {
 
   ///
   static Future<void> dispose() async {
-    _web3app = null;
-    if (_w3mService?.isConnected ?? false) {
-      await _w3mService!.disconnect();
-    }
+    if (_w3mService?.isConnected ?? false) await _w3mService!.disconnect();
     _w3mService = null;
   }
 
   ///
   static Future<bool> authorize(BuildContext context) async {
-    await dispose(); // TODO(Alex): is it necessary?
     await prepare();
     try {
       await _w3mService!.openModal(context);
       logInfo(info: 'Modal closed, connection = ${_w3mService!.isConnected}');
       if (!_w3mService!.isConnected) {
         logInfo(info: 'Connection cancelled');
+        await dispose();
         return false;
       }
 
@@ -78,7 +49,7 @@ final class AuthModalSign {
 
       _w3mService!.launchConnectedWallet();
 
-      final String? signedMessage = await _web3app!.request(
+      final String? signedMessage = await _w3mService!.request(
         chainId: 'eip155:1',
         topic: _w3mService!.session!.topic ?? '',
         request: SessionRequestParams(
@@ -89,16 +60,29 @@ final class AuthModalSign {
 
       if (signedMessage == null) {
         logInfo(info: 'Authorization cancelled');
+        await dispose();
         return false;
       }
 
-      logInfo(info: 'Authorization succeed');
-      logInfo(info: 'Address ${_w3mService!.session!.address}');
-      logInfo(info: 'signature $signedMessage');
-      return true;
+      if (signedMessage.startsWith('0x')) {
+        logInfo(info: 'Authorization succeed');
+        logInfo(info: 'Address ${_w3mService!.session!.address}');
+        logInfo(info: 'signature $signedMessage');
+        await dispose();
+        return true;
+      } else {
+        /// Got error
+        logInfo(info: 'Authorization failed');
+        logInfo(info: 'Address ${_w3mService!.session!.address}');
+        logInfo(info: 'Error $signedMessage');
+        await dispose();
+        return true;
+      }
     } catch (error) {
       logError(error: error.toString());
-      return false;
+
+      await dispose();
+      return true;
     }
   }
 }
